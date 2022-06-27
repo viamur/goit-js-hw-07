dd9fd0cbcf037c84b84d3db4b486e4204f46564d
Moskalenko Serhii <mynameserhii@gmail.com>
2022-06-27 20:32:15 +0300

    50%

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryDivRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const allItemsHtml = galleryItems
+  .map(({ description, original, preview }) => {
+    const htmlCode = `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    return htmlCode;
+  })
+  .join('');
+
+galleryDivRef.innerHTML = allItemsHtml;
+
+galleryDivRef.addEventListener('click', event => {
+  event.preventDefault();
+  const elem = event.target;
+
+  if (elem.nodeName !== 'IMG') {
+    return;
+  }
+
+  elem.src = elem.dataset.source;
+
+  const modal = basicLightbox.create(elem.outerHTML);
+  modal.show(addListenerOnKeyDown());
+
+  function addListenerOnKeyDown() {
+    document.addEventListener('keydown', onModalClosewichKeyDown);
+  }
+
+  function onModalClosewichKeyDown(event) {
+    if (event.code === 'Escape') {
+      modal.close();
+      document.removeEventListener('keydown', onModalClosewichKeyDown);
+    }
+  }
+});
