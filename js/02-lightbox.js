a500bc594212c703bd165c328bcb64c89ed0b65f
Moskalenko Serhii <mynameserhii@gmail.com>
2022-06-27 21:40:08 +0300

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDivRef = document.querySelector('.gallery');
+
+const allItemsHtml = galleryItems
+  .map(({ description, original, preview }) => {
+    const htmlCode = `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview} " alt="${description}" />
+</a>`;
+    return htmlCode;
+  })
+  .join('');
+
+galleryDivRef.innerHTML = allItemsHtml;
+
+const lightbox = new SimpleLightbox('.gallery a');
+lightbox.options.captionsData = 'alt';
+lightbox.options.captionDelay = 250;
